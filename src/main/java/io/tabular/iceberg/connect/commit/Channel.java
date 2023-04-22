b0483f01f69484644a1c791583ec14541a8f9bf7
Bryan Keller <bryanck@gmail.com>
2023-04-22 09:34:57 -0700

    durable queue

--- src/main/java/io/tabular/iceberg/connect/commit/Channel.java
+++ src/main/java/io/tabular/iceberg/connect/commit/Channel.java
@@ -1,15 +1,10 @@
 // Copyright 2023 Tabular Technologies Inc.
 package io.tabular.iceberg.connect.commit;
 
-import java.time.Duration;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
-import java.util.concurrent.ConcurrentLinkedQueue;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
 import lombok.SneakyThrows;
 import lombok.extern.log4j.Log4j;
 import org.apache.iceberg.util.PropertyUtil;
@@ -27,25 +22,17 @@ public abstract class Channel {
 
   protected final Map<String, String> kafkaProps;
   private final String coordinatorTopic;
-  private final ExecutorService executor;
-  private volatile boolean executorTerminated;
-  private final ConcurrentLinkedQueue<byte[]> queue;
   private final KafkaProducer<byte[], byte[]> producer;
+  private final KafkaConsumer<byte[], byte[]> consumer;
 
   private static final String COORDINATOR_TOPIC_PROP = "iceberg.coordinator.topic";
   private static final String KAFKA_PROP_PREFIX = "iceberg.kafka.";
-  private static final int CONSUMER_POLL_TIMEOUT_MS = 500;
-  private static final int EXEC_SHUTDOWN_WAIT_MS = 5000;
 
   public Channel(Map<String, String> props) {
     this.kafkaProps = PropertyUtil.propertiesWithPrefix(props, KAFKA_PROP_PREFIX);
     this.coordinatorTopic = props.get(COORDINATOR_TOPIC_PROP);
-    this.executor = Executors.newSingleThreadExecutor();
-    this.queue = new ConcurrentLinkedQueue<>();
-
-    Map<String, Object> producerProps = new HashMap<>(kafkaProps);
-    this.producer =
-        new KafkaProducer<>(producerProps, new ByteArraySerializer(), new ByteArraySerializer());
+    this.producer = createProducer();
+    this.consumer = createConsumer();
   }
 
   protected void send(Message message) {
@@ -57,45 +44,43 @@ public abstract class Channel {
 
   protected abstract void receive(Message message);
 
+  @SuppressWarnings("deprecation")
   public void process() {
-    while (queue.peek() != null) {
-      Message message = SerializationUtil.deserializeFromBytes(queue.remove());
-      log.info("Received message of type: " + message.getType().name());
-      receive(message);
+    // TODO: we're using the deprecated poll(long) API as it waits for metadata, better options?
+    ConsumerRecords<byte[], byte[]> records = consumer.poll(0);
+    while (!records.isEmpty()) {
+      records.forEach(
+          record -> {
+            Message message = SerializationUtil.deserializeFromBytes(record.value());
+            log.info("Received message of type: " + message.getType().name());
+            receive(message);
+          });
+      records = consumer.poll(0);
     }
   }
 
-  public void start() {
-    log.info("Channel starting");
-    executorTerminated = false;
-    executor.submit(
-        () -> {
-          // TODO: consumer cleanup/close
-          KafkaConsumer<byte[], byte[]> consumer = createConsumer();
-          consumer.subscribe(List.of(coordinatorTopic));
-          while (!executorTerminated) {
-            ConsumerRecords<byte[], byte[]> records =
-                consumer.poll(Duration.ofMillis(CONSUMER_POLL_TIMEOUT_MS));
-            records.forEach(record -> queue.add(record.value()));
-            consumer.commitSync();
-          }
-        });
+  private KafkaProducer<byte[], byte[]> createProducer() {
+    Map<String, Object> producerProps = new HashMap<>(kafkaProps);
+    return new KafkaProducer<>(producerProps, new ByteArraySerializer(), new ByteArraySerializer());
   }
 
   private KafkaConsumer<byte[], byte[]> createConsumer() {
     Map<String, Object> consumerProps = new HashMap<>(kafkaProps);
+    consumerProps.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false);
     consumerProps.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest");
     consumerProps.put(ConsumerConfig.GROUP_ID_CONFIG, "cg-iceberg-" + UUID.randomUUID());
     return new KafkaConsumer<>(
         consumerProps, new ByteArrayDeserializer(), new ByteArrayDeserializer());
   }
 
+  public void start() {
+    consumer.subscribe(List.of(coordinatorTopic));
+  }
+
   @SneakyThrows
   public void stop() {
     log.info("Channel stopping");
     producer.close();
-    executor.shutdown();
-    executorTerminated = true;
-    executor.awaitTermination(EXEC_SHUTDOWN_WAIT_MS, TimeUnit.MILLISECONDS);
+    consumer.close();
   }
 }
