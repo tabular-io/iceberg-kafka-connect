391ac10d53d4e8c953efa160a878de49f61722d6
Bryan Keller <bryanck@gmail.com>
2023-05-02 16:05:28 -0700

    push to docker hub

--- image.sh
+++ image.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
 
-# first create the ECR repo by adding it to 01_root/ecr.tf and applying
-
-export AWS_PROFILE=infra
-
-aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin 561839024272.dkr.ecr.us-west-2.amazonaws.com
-
 ./gradlew -xtest clean build
 
 docker buildx build  \
   --platform linux/amd64,linux/arm64 \
   --pull --push \
-  -t 561839024272.dkr.ecr.us-west-2.amazonaws.com/kafka-connect:latest \
+  -t tabulario/kafka-connect:latest \
   -f Dockerfile .
