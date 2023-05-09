48bf5a94ee8d34916a098c63b68f98bc4314b1c5
Bryan Keller <bryanck@gmail.com>
2023-05-09 10:31:15 -0700

    Revert "s3 publish"
    This reverts commit 6210a1babd72b44268c42738ca41b9bb345927df.

--- publish.sh
+++ publish.sh
@@ -7,5 +7,3 @@ docker buildx build  \
   --pull --push \
   -t tabulario/iceberg-kafka-connect:latest \
   -f Dockerfile .
-
-aws --profile infra s3 cp --recursive kafka-connect-runtime/build/distributions/ s3://tabular-repository-public/kc/
