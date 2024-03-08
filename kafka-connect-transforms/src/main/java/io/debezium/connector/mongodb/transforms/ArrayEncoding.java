f9f898874b443b2ef319bd2622e21764305635da
Mathew Fournier <mathew.fournier@tabular.io>
2024-03-08 11:35:17 -0800

    formatting and exclusion battles

--- kafka-connect-transforms/src/main/java/io/debezium/connector/mongodb/transforms/ArrayEncoding.java
+++ kafka-connect-transforms/src/main/java/io/debezium/connector/mongodb/transforms/ArrayEncoding.java
@@ -5,51 +5,51 @@
  */
 package io.debezium.connector.mongodb.transforms;
 
-public enum ArrayEncoding  {
-    ARRAY("array"),
-    DOCUMENT("document");
+public enum ArrayEncoding {
+  ARRAY("array"),
+  DOCUMENT("document");
 
-    private final String value;
+  private final String value;
 
-    ArrayEncoding(String value) {
-        this.value = value;
+  ArrayEncoding(String value) {
+    this.value = value;
+  }
+
+  public String getValue() {
+    return value;
+  }
+
+  /**
+   * Determine if the supplied value is one of the predefined options.
+   *
+   * @param value the configuration property value; may not be null
+   * @return the matching option, or null if no match is found
+   */
+  public static ArrayEncoding parse(String value) {
+    if (value == null) {
+      return null;
     }
-
-    public String getValue() {
-        return value;
+    value = value.trim();
+    for (ArrayEncoding option : ArrayEncoding.values()) {
+      if (option.getValue().equalsIgnoreCase(value)) {
+        return option;
+      }
     }
+    return null;
+  }
 
-    /**
-     * Determine if the supplied value is one of the predefined options.
-     *
-     * @param value the configuration property value; may not be null
-     * @return the matching option, or null if no match is found
-     */
-    public static ArrayEncoding parse(String value) {
-        if (value == null) {
-            return null;
-        }
-        value = value.trim();
-        for (ArrayEncoding option : ArrayEncoding.values()) {
-            if (option.getValue().equalsIgnoreCase(value)) {
-                return option;
-            }
-        }
-        return null;
+  /**
+   * Determine if the supplied value is one of the predefined options.
+   *
+   * @param value the configuration property value; may not be null
+   * @param defaultValue the default value; may be null
+   * @return the matching option, or null if no match is found and the non-null default is invalid
+   */
+  public static ArrayEncoding parse(String value, String defaultValue) {
+    ArrayEncoding mode = parse(value);
+    if (mode == null && defaultValue != null) {
+      mode = parse(defaultValue);
     }
-
-    /**
-     * Determine if the supplied value is one of the predefined options.
-     *
-     * @param value        the configuration property value; may not be null
-     * @param defaultValue the default value; may be null
-     * @return the matching option, or null if no match is found and the non-null default is invalid
-     */
-    public static ArrayEncoding parse(String value, String defaultValue) {
-        ArrayEncoding mode = parse(value);
-        if (mode == null && defaultValue != null) {
-            mode = parse(defaultValue);
-        }
-        return mode;
-    }
+    return mode;
+  }
 }
--- kafka-connect-transforms/src/test/java/io/debezium/connector/mongodb/transforms/MongoDataConverterTest.java
+++ kafka-connect-transforms/src/test/java/io/debezium/connector/mongodb/transforms/MongoDataConverterTest.java
@@ -15,7 +15,6 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Map.Entry;
 import java.util.TimeZone;
-
 import org.apache.kafka.connect.data.Schema;
 import org.apache.kafka.connect.data.SchemaBuilder;
 import org.apache.kafka.connect.data.Struct;
@@ -32,177 +31,209 @@ import org.junit.jupiter.api.Test;
  */
 public class MongoDataConverterTest {
 
-    private String record;
-    private BsonDocument val;
-    private SchemaBuilder builder;
-    private MongoDataConverter converter;
+  private String record;
+  private BsonDocument val;
+  private SchemaBuilder builder;
+  private MongoDataConverter converter;
 
+  @BeforeEach
+  public void setup() throws Exception {
+    record = getFile("restaurants5.json");
+    val = BsonDocument.parse(record);
+    builder = SchemaBuilder.struct().name("pub");
+    converter = new MongoDataConverter(ArrayEncoding.ARRAY);
+  }
 
-    @BeforeEach
-    public void setup() throws Exception {
-        record = getFile("restaurants5.json");
-        val = BsonDocument.parse(record);
-        builder = SchemaBuilder.struct().name("pub");
-        converter = new MongoDataConverter(ArrayEncoding.ARRAY);
+  @Test
+  public void shouldCreateCorrectStructFromInsertJson() {
+    for (Entry<String, BsonValue> entry : val.entrySet()) {
+      converter.addFieldSchema(entry, builder);
     }
 
-    @Test
-    public void shouldCreateCorrectStructFromInsertJson() {
-        for (Entry<String, BsonValue> entry : val.entrySet()) {
-            converter.addFieldSchema(entry, builder);
-        }
+    Schema finalSchema = builder.build();
+    Struct struct = new Struct(finalSchema);
 
-        Schema finalSchema = builder.build();
-        Struct struct = new Struct(finalSchema);
-
-        for (Entry<String, BsonValue> entry : val.entrySet()) {
-            converter.convertRecord(entry, finalSchema, struct);
-        }
-
-        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
-
-        assertThat(struct.toString()).isEqualTo(
-                "Struct{"
-                        + "address=Struct{"
-                        + "building=1007,"
-                        + "floor=Struct{"
-                        + "level=17,"
-                        + "description=level 17"
-                        + "},"
-                        + "coord=[-73.856077, 40.848447],"
-                        + "street=Morris Park Ave,"
-                        + "zipcode=10462"
-                        + "},"
-                        + "borough=Bronx,"
-                        + "cuisine=Bakery,"
-                        + "grades=["
-                        + "Struct{date=Mon Mar 03 00:00:00 UTC 2014,grade=A,score=2}, "
-                        + "Struct{date=Wed Sep 11 00:00:00 UTC 2013,grade=A,score=6}, "
-                        + "Struct{date=Thu Jan 24 00:00:00 UTC 2013,grade=A,score=10}, "
-                        + "Struct{date=Wed Nov 23 00:00:00 UTC 2011,grade=A,score=9}, "
-                        + "Struct{date=Thu Mar 10 00:00:00 UTC 2011,grade=B,score=14}"
-                        + "],"
-                        + "name=Morris Park Bake Shop,"
-                        + "restaurant_id=30075445"
-                        + "}");
+    for (Entry<String, BsonValue> entry : val.entrySet()) {
+      converter.convertRecord(entry, finalSchema, struct);
     }
 
-    @Test
-    public void shouldCreateCorrectSchemaFromInsertJson() {
-        for (Entry<String, BsonValue> entry : val.entrySet()) {
-            converter.addFieldSchema(entry, builder);
-        }
-        Schema finalSchema = builder.build();
+    TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
 
-        assertThat(finalSchema).isEqualTo(
-                SchemaBuilder.struct().name("pub")
-                        .field("address", SchemaBuilder.struct().name("pub.address").optional()
-                                .field("building", Schema.OPTIONAL_STRING_SCHEMA)
-                                .field("floor", SchemaBuilder.struct().name("pub.address.floor").optional()
-                                        .field("level", Schema.OPTIONAL_INT32_SCHEMA)
-                                        .field("description", Schema.OPTIONAL_STRING_SCHEMA)
-                                        .build())
-                                .field("coord", SchemaBuilder.array(Schema.OPTIONAL_FLOAT64_SCHEMA).optional().build())
-                                .field("street", Schema.OPTIONAL_STRING_SCHEMA)
-                                .field("zipcode", Schema.OPTIONAL_STRING_SCHEMA)
-                                .build())
-                        .field("borough", Schema.OPTIONAL_STRING_SCHEMA)
-                        .field("cuisine", Schema.OPTIONAL_STRING_SCHEMA)
-                        .field("grades", SchemaBuilder.array(SchemaBuilder.struct().name("pub.grades").optional()
-                                        .field("date", Timestamp.builder().optional().build())
-                                        .field("grade", Schema.OPTIONAL_STRING_SCHEMA)
-                                        .field("score", Schema.OPTIONAL_INT32_SCHEMA)
-                                        .build())
+    assertThat(struct.toString())
+        .isEqualTo(
+            "Struct{"
+                + "address=Struct{"
+                + "building=1007,"
+                + "floor=Struct{"
+                + "level=17,"
+                + "description=level 17"
+                + "},"
+                + "coord=[-73.856077, 40.848447],"
+                + "street=Morris Park Ave,"
+                + "zipcode=10462"
+                + "},"
+                + "borough=Bronx,"
+                + "cuisine=Bakery,"
+                + "grades=["
+                + "Struct{date=Mon Mar 03 00:00:00 UTC 2014,grade=A,score=2}, "
+                + "Struct{date=Wed Sep 11 00:00:00 UTC 2013,grade=A,score=6}, "
+                + "Struct{date=Thu Jan 24 00:00:00 UTC 2013,grade=A,score=10}, "
+                + "Struct{date=Wed Nov 23 00:00:00 UTC 2011,grade=A,score=9}, "
+                + "Struct{date=Thu Mar 10 00:00:00 UTC 2011,grade=B,score=14}"
+                + "],"
+                + "name=Morris Park Bake Shop,"
+                + "restaurant_id=30075445"
+                + "}");
+  }
+
+  @Test
+  public void shouldCreateCorrectSchemaFromInsertJson() {
+    for (Entry<String, BsonValue> entry : val.entrySet()) {
+      converter.addFieldSchema(entry, builder);
+    }
+    Schema finalSchema = builder.build();
+
+    assertThat(finalSchema)
+        .isEqualTo(
+            SchemaBuilder.struct()
+                .name("pub")
+                .field(
+                    "address",
+                    SchemaBuilder.struct()
+                        .name("pub.address")
+                        .optional()
+                        .field("building", Schema.OPTIONAL_STRING_SCHEMA)
+                        .field(
+                            "floor",
+                            SchemaBuilder.struct()
+                                .name("pub.address.floor")
                                 .optional()
+                                .field("level", Schema.OPTIONAL_INT32_SCHEMA)
+                                .field("description", Schema.OPTIONAL_STRING_SCHEMA)
                                 .build())
-                        .field("name", Schema.OPTIONAL_STRING_SCHEMA)
-                        .field("restaurant_id", Schema.OPTIONAL_STRING_SCHEMA)
-                        .build());
-    }
-
-    private String getFile(String fileName) throws IOException, URISyntaxException {
-        URL jsonResource = getClass().getClassLoader().getResource(fileName);
-        return new String(
-                Files.readAllBytes(Paths.get(jsonResource.toURI())),
-                StandardCharsets.UTF_8);
-    }
-
-    @Test
-    public void shouldProcessNullValue() {
-        val = BsonDocument.parse("{\n" +
-                "    \"_id\" : ObjectId(\"51e5619ee4b01f9fbdfba9fc\"),\n" +
-                "    \"delivery\" : {\n" +
-                "        \"hour\" : null,\n" +
-                "        \"hourId\" : 10\n" +
-                "    }\n" +
-                "}");
-        builder = SchemaBuilder.struct().name("withnull");
-        converter = new MongoDataConverter(ArrayEncoding.ARRAY);
-
-        for (Entry<String, BsonValue> entry : val.entrySet()) {
-            converter.addFieldSchema(entry, builder);
-        }
-        Schema finalSchema = builder.build();
-        Struct struct = new Struct(finalSchema);
-
-        for (Entry<String, BsonValue> entry : val.entrySet()) {
-            converter.convertRecord(entry, finalSchema, struct);
-        }
-
-        assertThat(finalSchema).isEqualTo(
-                SchemaBuilder.struct().name("withnull")
-                        .field("_id", Schema.OPTIONAL_STRING_SCHEMA)
-                        .field("delivery", SchemaBuilder.struct().name("withnull.delivery").optional()
-                                .field("hour", Schema.OPTIONAL_STRING_SCHEMA)
-                                .field("hourId", Schema.OPTIONAL_INT32_SCHEMA)
+                        .field(
+                            "coord",
+                            SchemaBuilder.array(Schema.OPTIONAL_FLOAT64_SCHEMA).optional().build())
+                        .field("street", Schema.OPTIONAL_STRING_SCHEMA)
+                        .field("zipcode", Schema.OPTIONAL_STRING_SCHEMA)
+                        .build())
+                .field("borough", Schema.OPTIONAL_STRING_SCHEMA)
+                .field("cuisine", Schema.OPTIONAL_STRING_SCHEMA)
+                .field(
+                    "grades",
+                    SchemaBuilder.array(
+                            SchemaBuilder.struct()
+                                .name("pub.grades")
+                                .optional()
+                                .field("date", Timestamp.builder().optional().build())
+                                .field("grade", Schema.OPTIONAL_STRING_SCHEMA)
+                                .field("score", Schema.OPTIONAL_INT32_SCHEMA)
                                 .build())
-                        .build());
-        assertThat(struct.toString()).isEqualTo(
-                "Struct{"
-                        + "_id=51e5619ee4b01f9fbdfba9fc,"
-                        + "delivery=Struct{"
-                        + "hourId=10"
-                        + "}"
-                        + "}");
+                        .optional()
+                        .build())
+                .field("name", Schema.OPTIONAL_STRING_SCHEMA)
+                .field("restaurant_id", Schema.OPTIONAL_STRING_SCHEMA)
+                .build());
+  }
+
+  private String getFile(String fileName) throws IOException, URISyntaxException {
+    URL jsonResource = getClass().getClassLoader().getResource(fileName);
+    return new String(Files.readAllBytes(Paths.get(jsonResource.toURI())), StandardCharsets.UTF_8);
+  }
+
+  @Test
+  public void shouldProcessNullValue() {
+    val =
+        BsonDocument.parse(
+            "{\n"
+                + "    \"_id\" : ObjectId(\"51e5619ee4b01f9fbdfba9fc\"),\n"
+                + "    \"delivery\" : {\n"
+                + "        \"hour\" : null,\n"
+                + "        \"hourId\" : 10\n"
+                + "    }\n"
+                + "}");
+    builder = SchemaBuilder.struct().name("withnull");
+    converter = new MongoDataConverter(ArrayEncoding.ARRAY);
+
+    for (Entry<String, BsonValue> entry : val.entrySet()) {
+      converter.addFieldSchema(entry, builder);
+    }
+    Schema finalSchema = builder.build();
+    Struct struct = new Struct(finalSchema);
+
+    for (Entry<String, BsonValue> entry : val.entrySet()) {
+      converter.convertRecord(entry, finalSchema, struct);
     }
 
-    @Test
-    public void shouldProcessUnsupportedValue() {
-        val = BsonDocument.parse("{\n" +
-                "    \"_id\" : ObjectId(\"518cc94bc27cfa20d9693e5d\"),\n" +
-                "    \"name\" : undefined,\n" +
-                "    \"address\" : {\n" +
-                "        \"building\" : undefined,\n" +
-                "        \"floor\" : 10\n" +
-                "    }\n" +
-                "}");
-        builder = SchemaBuilder.struct().name("withundefined");
-        converter = new MongoDataConverter(ArrayEncoding.DOCUMENT);
+    assertThat(finalSchema)
+        .isEqualTo(
+            SchemaBuilder.struct()
+                .name("withnull")
+                .field("_id", Schema.OPTIONAL_STRING_SCHEMA)
+                .field(
+                    "delivery",
+                    SchemaBuilder.struct()
+                        .name("withnull.delivery")
+                        .optional()
+                        .field("hour", Schema.OPTIONAL_STRING_SCHEMA)
+                        .field("hourId", Schema.OPTIONAL_INT32_SCHEMA)
+                        .build())
+                .build());
+    assertThat(struct.toString())
+        .isEqualTo(
+            "Struct{"
+                + "_id=51e5619ee4b01f9fbdfba9fc,"
+                + "delivery=Struct{"
+                + "hourId=10"
+                + "}"
+                + "}");
+  }
 
-        for (Entry<String, BsonValue> entry : val.entrySet()) {
-            converter.addFieldSchema(entry, builder);
-        }
-        Schema finalSchema = builder.build();
-        Struct struct = new Struct(finalSchema);
+  @Test
+  public void shouldProcessUnsupportedValue() {
+    val =
+        BsonDocument.parse(
+            "{\n"
+                + "    \"_id\" : ObjectId(\"518cc94bc27cfa20d9693e5d\"),\n"
+                + "    \"name\" : undefined,\n"
+                + "    \"address\" : {\n"
+                + "        \"building\" : undefined,\n"
+                + "        \"floor\" : 10\n"
+                + "    }\n"
+                + "}");
+    builder = SchemaBuilder.struct().name("withundefined");
+    converter = new MongoDataConverter(ArrayEncoding.DOCUMENT);
 
-        for (Entry<String, BsonValue> entry : val.entrySet()) {
-            converter.convertRecord(entry, finalSchema, struct);
-        }
-
-        assertThat(finalSchema).isEqualTo(
-                SchemaBuilder.struct().name("withundefined")
-                        .field("_id", Schema.OPTIONAL_STRING_SCHEMA)
-                        .field("address", SchemaBuilder.struct().name("withundefined.address").optional()
-                                .field("floor", Schema.OPTIONAL_INT32_SCHEMA)
-                                .build())
-                        .build());
-        assertThat(struct.toString()).isEqualTo(
-                "Struct{"
-                        + "_id=518cc94bc27cfa20d9693e5d,"
-                        + "address=Struct{"
-                        + "floor=10"
-                        + "}"
-                        + "}");
-
+    for (Entry<String, BsonValue> entry : val.entrySet()) {
+      converter.addFieldSchema(entry, builder);
     }
+    Schema finalSchema = builder.build();
+    Struct struct = new Struct(finalSchema);
+
+    for (Entry<String, BsonValue> entry : val.entrySet()) {
+      converter.convertRecord(entry, finalSchema, struct);
+    }
+
+    assertThat(finalSchema)
+        .isEqualTo(
+            SchemaBuilder.struct()
+                .name("withundefined")
+                .field("_id", Schema.OPTIONAL_STRING_SCHEMA)
+                .field(
+                    "address",
+                    SchemaBuilder.struct()
+                        .name("withundefined.address")
+                        .optional()
+                        .field("floor", Schema.OPTIONAL_INT32_SCHEMA)
+                        .build())
+                .build());
+    assertThat(struct.toString())
+        .isEqualTo(
+            "Struct{"
+                + "_id=518cc94bc27cfa20d9693e5d,"
+                + "address=Struct{"
+                + "floor=10"
+                + "}"
+                + "}");
+  }
 }
